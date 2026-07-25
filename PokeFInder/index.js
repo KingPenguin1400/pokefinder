62641bfaba0f1408b8c310c2b9b29f4ab3e42aef
KingPenguin1400 <adrian.a.gastelo1@gmail.com>
2026-07-24 20:35:35 -0400

    Enhance fetchData to include shiny and back sprites
    Added functionality to display shiny and back sprites for Pokémon.

--- PokeFInder/index.js
+++ PokeFInder/index.js
@@ -10,11 +10,23 @@ async function fetchData(){
         }
 
         const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
+        const pokemonSpriteF = data.sprites.front_default;
+        const pokemonSpriteFShiny = data.sprites.front_shiny;
+        const pokemonSpriteB = data.sprites.back_default;
+        const pokemonSpriteBShiny = data.sprites.back_shiny;
         const imgElement = document.getElementById("pokemonSprite");
+        const imgElement2 = document.getElementById("img2");
+        const imgElement3 = document.getElementById("img3");
+        const imgElement4 = document.getElementById("img4");
         
-        imgElement.src = pokemonSprite;
+        imgElement.src = pokemonSpriteF;
         imgElement.style.display = "block";
+        imgElement2.src = pokemonSpriteFShiny;
+        imgElement2.style.display = "block";
+        imgElement3.src = pokemonSpriteB;
+        imgElement3.style.display = "block";
+        imgElement4.src = pokemonSpriteBShiny;
+        imgElement4.style.display = "block";
         
         console.log(data);
     }
